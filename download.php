8f15864edffe46c3dc6991c7e5b4685489afe388
Daniele Scasciafratte <mte90net@gmail.com>
2016-04-21 12:24:00 +0200

    new system

---
+++ download.php
@@ -0,0 +1,51 @@
+#!/usr/bin/php
+<?php
+echo 'Download it_IT' . "...\n";
+echo shell_exec('./convert-it_IT.py');
+go_download_glotdict('bg_BG', "https://translate.wordpress.org/projects/wp/dev/bg/default/glossary");
+go_download_glotdict('de_DE', "https://translate.wordpress.org/projects/wp/dev/de/default/glossary");
+go_download_glotdict('es_ES', "https://translate.wordpress.org/projects/wp/dev/es/default/glossary");
+go_download_glotdict('fi', "https://translate.wordpress.org/projects/wp/dev/fi/default/glossary");
+go_download_glotdict('fr_FR', "https://translate.wordpress.org/projects/wp/dev/fr/default/glossary");
+go_download_glotdict('hi_IN', "https://translate.wordpress.org/projects/wp/dev/hi/default/glossary");
+go_download_glotdict('lt_LT', "https://translate.wordpress.org/projects/wp/dev/lt/default/glossary");
+go_download_glotdict('ja', "https://translate.wordpress.org/projects/wp/dev/ja/default/glossary");
+go_download_glotdict('nl_NL', "https://translate.wordpress.org/projects/wp/dev/nl/default/glossary");
+go_download_glotdict('sk_SK', "https://translate.wordpress.org/projects/wp/dev/sk/default/glossary");
+go_download_glotdict('sv_SE', "https://translate.wordpress.org/projects/wp/dev/sv/default/glossary");
+
+
+function go_download_glotdict($locale, $url) {
+        $response = array();
+        $lines = '';
+        echo 'Download ' . $locale . "...\n";
+        $doc1 = file_get_contents($url);
+        if ($doc1 !== false) {
+            $doc = new DOMDocument;
+            @$doc->loadHTML($doc1);
+            $doc->preserveWhiteSpace = false;
+            $classname = 'view';
+            $xpath = new DOMXPath($doc);
+            $rows = $xpath->query("//*[@class='" . $classname . "']");
+            if ($rows->length > 2) {
+                $firstrow = true;
+                foreach ($rows as $row) {
+                    if ($firstrow) {
+                        $firstrow = false;
+                    } else {
+                        $lines = $lines . ',';
+                    }
+                    $all_info = $row->getElementsByTagName('td');
+                    $line = array();
+                    $line[$all_info->item(0)->nodeValue] = array('comment' => $all_info->item(3)->nodeValue, 'pos' => '', 'translation' => $all_info->item(2)->nodeValue);
+                    $lines = $lines . substr(json_encode($line, JSON_PRETTY_PRINT),1,-1);
+                }
+                file_put_contents('./dictionaries/' . $locale.'.json' , '{' . $lines . '}');
+                echo $rows->length . ' Glossary terms' . "\n";
+            } else {
+                echo ' No rows found on page ' . $url . "\n";
+            }
+        } else {
+            echo 'Page ' . $url . ' not responding...' . "\n";
+        }
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
-##Why
-The [Italian Glossary](https://docs.google.com/spreadsheets/d/1eleJcaX5ZOxAlaDiFMHk_uETenEQI8-1Rg99Jpll3mc/edit#gid=0) for the WordPress translators is on Google Drive and convert that for [GlotDict](https://github.com/Mte90/GlotDict) is a manually procedure.  
-That simple script convert automatically for GlotDict. Enjoy!
+##What
+That simple script convert the glossaries on glotpress automatically for GlotDict. Enjoy!
+
+##Why python
+The [Italian Glossary](https://docs.google.com/spreadsheets/d/1eleJcaX5ZOxAlaDiFMHk_uETenEQI8-1Rg99Jpll3mc/edit#gid=0) is on Google Drive so require specific workaround.  
+
 
 ##Install
 
@@ -13,9 +16,14 @@ pip install gspread
 ##Execute
 
 ```
-./convert.py
+./download.php
 ```
 
-##Generate the OAuth key
+##Generate the OAuth key for python
 Follow the instructions on [http://gspread.readthedocs.org/en/latest/oauth2.html](http://gspread.readthedocs.org/en/latest/oauth2.html), rename the file in credentials.json and put in the folder.   
 Finally add the client_email of the file in the online document.
+
+# Contributors
+
+* [Daniele Scasciafratte](https://github.com/Mte90) - The developer
+* Pascal Casier - For the php script
